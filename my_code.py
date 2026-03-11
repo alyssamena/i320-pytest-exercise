8eef8a49f77f029924fa4e3abeec7f18e7f5e00c
Alyssa Mena <alyssamena5204@yahoo.com>
2026-03-11 10:35:45 -0500

    sanitycheck

--- my_code.py
+++ my_code.py
@@ -4,5 +4,3 @@ def fix_phone_num(phone_num_to_fix):
   # can only handle numbers that are exactly 10 digits long
   if (len(phone_num_to_fix) != 10):
     raise ValueError("Can only format numbers that are exactly 10 digits long")
-  
-  ...
